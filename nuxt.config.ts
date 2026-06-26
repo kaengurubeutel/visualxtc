96f8a46fa553ad23cc2c514194e35f14f7c29f66
Maximilian Uhlig <68818935+kaengurubeutel@users.noreply.github.com>
2026-06-26 12:55:58 +0200

    Installed Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/a11y',
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/apollo',
+  ],
+
+  image: {
+    providers: {
+      hygraph: {
+        baseurl: "https://media.graphassets.com"
+      }
+    }
+  }
+
+})
