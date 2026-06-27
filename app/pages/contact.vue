c72f9b302d5c849996fb7e025bc3d5152511b96f
Maximilian Uhlig <68818935+kaengurubeutel@users.noreply.github.com>
2026-06-27 11:12:12 +0200

    Structure #1 added Routes

---
+++ app/pages/contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    das sind meine kontaktdaten
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,10 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    rules: {
+      // Schaltet die Multi-Word-Regel komplett aus
+      'vue/multi-word-component-names': 'off'
+    }
+  }
 )
